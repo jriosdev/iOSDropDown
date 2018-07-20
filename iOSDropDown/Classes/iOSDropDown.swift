a0b40d69dec577c8a75526e971eee5b17b65c986
Jishnu Raj T <38908155+jriosdev@users.noreply.github.com>
2018-07-20 15:03:36 +0530

    Update iOSDropDown.swift

--- iOSDropDown/Classes/iOSDropDown.swift
+++ iOSDropDown/Classes/iOSDropDown.swift
@@ -105,27 +105,16 @@ public class DropDown : UITextField{
     fileprivate var TableDidDisappearCompletion: () -> () = { }
     
     func setupUI () {
-        
-        let size = self.frame.height
+       let size = self.frame.height
         let rightView = UIView(frame: CGRect(x: 0.0, y: 0.0, width: size - 2*arrowPadding, height: size))
-       
-       
         self.rightView = rightView
         self.rightViewMode = .always
-        let arrowContainerView = UIView(frame: CGRect(x: self.bounds.maxX - size,
-                                                      y: 0,
-                                                      width: size,
-                                                      height: size))
-        
-        self.addSubview(arrowContainerView)
-        
-        arrow = Arrow(origin: CGPoint(x: arrowPadding,
-                                      y: arrowPadding),
-                      size: arrowContainerView.frame.width - (arrowPadding*2))
+        let arrowContainerView = UIView(frame: rightView.frame)
+        self.rightView?.addSubview(arrowContainerView)
+        let arrowSize = arrowContainerView.frame.width - (arrowPadding*2)
+        arrow = Arrow(origin: CGPoint(x: arrowPadding,y: arrowContainerView.center.y-(arrowSize/2)),size: arrowSize)
         arrowContainerView.addSubview(arrow)
         addGesture()
-        
-        
     }
     fileprivate func addGesture (){
         let gesture =  UITapGestureRecognizer(target: self, action:  #selector(touchAction))
